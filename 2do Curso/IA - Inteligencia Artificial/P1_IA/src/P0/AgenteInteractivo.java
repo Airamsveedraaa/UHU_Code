ed5934f738a39c225e31e2752f954494ee73f550
Airamsveedraaa <airamsveedraaa@github.com>
2026-03-10 10:41:37 +0100

    Añadido agenteInteractivo (Tarea0)

--- 2do Curso/IA - Inteligencia Artificial/P1_IA/src/P0/AgenteInteractivo.java
+++ 2do Curso/IA - Inteligencia Artificial/P1_IA/src/P0/AgenteInteractivo.java
@@ -12,5 +12,42 @@ import java.util.Scanner;
 //==========================================
 class AgenteInteractivo{
 	
+	Scanner scanner = new Scanner(System.in);
+	
+	public String pensar(Entorno entorno) {
+	String accion="";
+	int fila,col;
+	
+	do {
+	System.out.println("Introduzca la accion a realizar: ");
+	 accion = scanner.nextLine();
+	 fila=entorno.agenteF;
+	 col=entorno.agenteC;
+	if(accion.equals("N")) {
+		fila--;
+		}
+	
+	else if(accion.equals("S")) {
+		fila++;
+		}
+	
+	
+	else if(accion.equals("E")) {
+		col++;
+		}
+	
+	
+	else if(accion.equals("O")) {
+		col--;
+	}
+	
+	if(!entorno.esTransitable(fila, col)) {
+		System.out.println("Acción no posible de realizar ");
+	}
+	}while(!entorno.esTransitable(fila,col));
+	// codigo Alumno 
+	return accion;
+	
+	}
 }
 
