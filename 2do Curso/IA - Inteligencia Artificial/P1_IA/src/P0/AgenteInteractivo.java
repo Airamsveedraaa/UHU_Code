2c08decc7bdb3feda09bd16c3fbb7cbe1445cc2d
Airamsveedraaa <airamsveedraaa@github.com>
2026-03-10 10:59:54 +0100

    Corregido Agente Greedy (Tarea 2C), ahora consigue resolver el laberinto

--- 2do Curso/IA - Inteligencia Artificial/P1_IA/src/P0/AgenteInteractivo.java
+++ 2do Curso/IA - Inteligencia Artificial/P1_IA/src/P0/AgenteInteractivo.java
@@ -103,7 +103,7 @@ class AgenteReactivo {
 		 n++;
 	 }
 	 if(n==0) {
-		 return "Z";
+		 return "N";
 	 }
 	 //escoger una accion aleatoria de todas las que tenemos
 	 int indice=generador.nextInt(n);
@@ -138,38 +138,83 @@ class AgenteGreedy{
 public String pensar(Entorno mapa) {
 	int fila=mapa.agenteF;
 	int col=mapa.agenteC;
+	 Random generador= new Random();
+
 	
-	String mejorAccion="";
 	int menor=Integer.MAX_VALUE;
 	int distancia;
+	int n=0; //indice para el array
+	String [] acciones= new String[4]; //acciones con menor distancia manhatan
+	
+	//Primera pasada para obtener la menor distancia
 	if(mapa.esTransitable(fila -1 , col)) {
 		distancia=Math.abs(fila-1 - mapa.metaF) + (Math.abs(col - mapa.metaC));
 		if(distancia < menor) {
 			menor=distancia;
-			mejorAccion="N";
+			
 		}
 	}
 	if(mapa.esTransitable(fila +1 , col)) {
 		distancia=Math.abs(fila+1 - mapa.metaF) + (Math.abs(col - mapa.metaC));
 		if(distancia < menor) {
 			menor=distancia;
-			mejorAccion="S";
+			
 		}
 	}
 	if(mapa.esTransitable(fila , col+1)) {
 		distancia=Math.abs(fila - mapa.metaF) + (Math.abs(col+1 - mapa.metaC));
 		if(distancia < menor) {
 			menor=distancia;
-			mejorAccion="E";
+			
 		}
 	}
 	if(mapa.esTransitable(fila , col-1)) {
 		distancia=Math.abs(fila - mapa.metaF) + (Math.abs(col-1 - mapa.metaC));
 		if(distancia < menor) {
 			menor=distancia;
-			mejorAccion="O";
 		}
 	}
-	return mejorAccion;
+	
+	if(mapa.esTransitable(fila -1 , col)) {
+		distancia=Math.abs(fila-1 - mapa.metaF) + (Math.abs(col - mapa.metaC));
+		if(distancia == menor) {
+			acciones[n]="N";
+			n++;
+			
+		}
+	}
+	if(mapa.esTransitable(fila +1 , col)) {
+		distancia=Math.abs(fila+1 - mapa.metaF) + (Math.abs(col - mapa.metaC));
+		if(distancia == menor) {
+			acciones[n]="S";
+			n++;
+			
+		}
+	}
+	if(mapa.esTransitable(fila , col+1)) {
+		distancia=Math.abs(fila - mapa.metaF) + (Math.abs(col+1 - mapa.metaC));
+		if(distancia == menor) {
+			acciones[n]="E";
+			n++;
+			
+		}
+	}
+	if(mapa.esTransitable(fila , col-1)) {
+		distancia=Math.abs(fila - mapa.metaF) + (Math.abs(col-1 - mapa.metaC));
+		if(distancia == menor) {
+			acciones[n]="O";
+			n++;
+		}
+	}
+	
+	//caso en el que no haya acciones posibles
+	if(n==0) {
+		 return "N";
+	 }
+	
+	//devolver uno aleatorio de los añadidos al array
+	 int indice=generador.nextInt(n);
+	 // --- FIN CODIGO ALUMNO ---
+	 return acciones[indice]; //”N” “S” “E” “0” 
 	}
 }
