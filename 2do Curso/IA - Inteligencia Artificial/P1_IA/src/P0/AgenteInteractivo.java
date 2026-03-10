e9b8792e938ec2c9405a53ff9d114693174aee84
Airamsveedraaa <airamsveedraaa@github.com>
2026-03-10 10:33:13 +0100

    Feat: añadida primera práctica de Inteligencia Artificial

---
+++ 2do Curso/IA - Inteligencia Artificial/P1_IA/src/P0/AgenteInteractivo.java
@@ -0,0 +1,16 @@
+package P0;
+
+import java.util.Random;
+import java.util.Scanner;
+//==========================================
+//TAREA 0: AGENTE INTERACTIVO
+//==========================================
+
+
+//==========================================
+//TAREA 0: AGENTE INTERACTIVO
+//==========================================
+class AgenteInteractivo{
+	
+}
+
---
+++ 2do Curso/IA - Inteligencia Artificial/P1_IA/src/P0/Tron_P0_BaseSimple.java
@@ -0,0 +1,68 @@
+package P0;
+
+import java.util.*;
+
+
+public class Tron_P0_BaseSimple {
+
+    // --- MAPAS DE PRUEBA ---
+    
+    // Mapa trampa: La casilla (1,1) al Este lleva a un callejón sin salida
+    static final String MAPA_CALLEJON =
+            "#########\n" +
+            "#A      #\n" + 
+            "# ##### #\n" + 
+            "# #   # #\n" + 
+            "#     S #\n" +
+            "#########";
+
+    static final String MAPA_GRANDE =
+            "####################\n" +
+            "#A  #              #\n" +
+            "# # # ###### ##### #\n" +
+            "# #   #      #     #\n" +
+            "# ##### ###### ### #\n" +
+            "#     #        #   #\n" +
+            "# ### ######## # # #\n" +
+            "#   #          #   #\n" +
+            "# ############## S #\n" +
+            "####################";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1. SELECCIONAR MAPA
+        // String mapaActual = MAPA_CALLEJON;
+        String mapaActual = MAPA_GRANDE;
+
+        Entorno juego = new Entorno(mapaActual);
+        
+     // 2. Elegir Agente (Descomentar el que se quiera probar)
+         //AgenteReactivo agente = new AgenteReactivo();
+        //AgenteDeliberativo agente = new AgenteDeliberativo(juego.filas, juego.cols);
+        AgenteGreedy agente= new AgenteGreedy();
+        System.out.println("--- Inicio de la Simulación ---");
+        int ciclos = 0;
+        boolean juegoActivo = true;
+        // BUCLE DE SIMULACIÓN
+        while (juegoActivo && ciclos < 200) {
+        juego.dibujar();
+        // El agente decide
+        String accion = agente.pensar(juego);
+        // El entorno aplica el movimiento
+        boolean movimientoExitoso = juego.moverAgente(accion);
+        System.out.println("Ciclo " + ciclos + " | Acción: " +
+       accion);
+
+        if (!movimientoExitoso) {
+        System.out.println(">>> ¡CRASH! El agente se ha chocado.");
+        juegoActivo = false;
+        } else if (juego.hemosGanado()) {
+        System.out.println(">>> ¡VICTORIA! Salida encontrada.");
+        juego.dibujar();
+        juegoActivo = false;
+        }
+        Thread.sleep(300); // Retardo para animación
+        ciclos++;
+        }
+        }
+       }
